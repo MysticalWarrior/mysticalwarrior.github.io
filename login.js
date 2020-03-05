af0fc436433daf6a24ffc5ad12b05bbcea885ca6
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2020-03-05 15:09:40 -0500

    v1.1.3
    v1.1.3
    login.js:
    - changed valid authCodes to be [0,1,2,3,4,5,6,7,8,9]
    - looped code checking to check all authCodes
    welcome.html:
    - put a line break between links
    - added a link to the Changelog //which has it formatted weirdly... will work on this site feature.
    Changelog.txt:
    - cloned contents to an html file
    Changelog.html:
    - created file and cloned the contents of Changelog.txt
    - updated changelog to specify the file being changed

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 
 let user;
 let isAuth = false;
-const CODE = 2;
+const CODES = [0,1,2,3,4,5,6,7,8,9]
 function login() {
     //alert(user)//troubleshooting
     if(!user) {
@@ -22,9 +22,18 @@ function getUser(){
 }
 
 function verifyCode(code){
+    /*//this code works when there is one code that works 
+    (so it is kept for later when there will be a single valid code for the user that is accessed at the call of the script.)
     if (+code == CODE){
         isAuth = true;
         document.getElementById('authState').innerText = "authorized";
         document.getElementById('btnLogin').innerText= "Proceed to site";
+    }*/
+    for(let i=0; i<CODES.length;i++){
+        if(+code == CODES[i]){ //the single "+" in front of code converts it to a number, its called a unary operator.
+            isAuth = true;
+            document.getElementById('authState').innerText = "authorized";
+            document.getElementById('btnLogin').innerText= "Proceed to site";
+        }
     }
 }
