5ea4ca5dbbdaa69c98655eb2a0ac0b3528df55e0
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2020-03-05 14:35:52 -0500

    v1.1.2
    v1.1.2
    - restructured login script into seperate functions
    - matched code structure across pages
    - added some links to the welcome page for some content at least...

--- login.js
+++ login.js
@@ -1,23 +1,30 @@
-    let user;
-	let isAuth = false;
-    function login() 
-    {
-		//alert(user)//troubleshooting
-		if(!user) 
-		{
-			user = prompt("Please enter username: ");
-			document.getElementById('user').innerText = "User: " + user;
-			document.getElementById('btnLogin').innerText= "Authorize";
-		}
-		else if(isAuth)
-		{
-			window.open("welcome.html");
-			//this.window.close();
-		}
-		else if(!isAuth && (prompt(`Please enter ${user}\'s code: `) == 2))
-		{
-			isAuth = true;
-			document.getElementById('authState').innerText = "authorized";
-			document.getElementById('btnLogin').innerText= "Proceed to site";
-		}
-	}
+
+let user;
+let isAuth = false;
+const CODE = 2;
+function login() {
+    //alert(user)//troubleshooting
+    if(!user) {
+        getUser();
+    }
+    else if(isAuth){
+        window.open("welcome.html");
+    }
+    else if(!isAuth){
+        verifyCode(prompt(`Please enter ${user}\'s code: `));
+    }
+}
+
+function getUser(){
+    user = prompt("Please enter username: ");
+    document.getElementById('user').innerText = "User: " + user;
+    document.getElementById('btnLogin').innerText= "Authorize";
+}
+
+function verifyCode(code){
+    if (+code == CODE){
+        isAuth = true;
+        document.getElementById('authState').innerText = "authorized";
+        document.getElementById('btnLogin').innerText= "Proceed to site";
+    }
+}
